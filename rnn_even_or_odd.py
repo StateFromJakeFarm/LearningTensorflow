9863e92ecfe4b064a6d310c74e37589e28e25e38
Jake Hansen <jhansen44@mail.csuchico.edu>
2018-05-29 21:41:04 +0200

    wip

--- rnn_even_or_odd.py
+++ rnn_even_or_odd.py
@@ -13,6 +13,9 @@ element_size = 1
 digit_to_word_map = {1:'One',2:'Two', 3:'Three', 4:'Four', 5:'Five',
                      6:'Six',7:'Seven',8:'Eight',9:'Nine'}
 digit_to_word_map[0] = 'PAD'
+word_to_ID_map = {'One': 1, 'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5,
+                  'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9}
+word_to_ID_map[0] = 0
 
 even_sentences = []
 odd_sentences = []
@@ -40,4 +43,26 @@ for i in range(10000):
 data = even_sentences + odd_sentences
 seqlens *= 2
 
-print(even_sentences[0:6])
+# Match data to labels
+labels = [1]*10000 + [0]*10000
+for i in range(len(labels)):
+    label = labels[i]
+    one_hot_encoding = [0]*2
+    one_hot_encoding[label] = 1
+    labels[i] = one_hot_encoding
+
+data_indices = list(range(len(data)))
+np.random.shuffle(data_indices)
+data = np.array(data)[data_indices]
+
+labels = np.array(labels)[data_indices]
+seqlens = np.array(seqlens)[data_indices]
+
+# Divide into training and testing sets
+train_x = data[:10000]
+train_y = labels[:10000]
+train_seqlens = seqlens[:10000]
+
+test_x = data[10000:]
+test_y = labels[10000:]
+test_seqlens = seqlens[10000:]
